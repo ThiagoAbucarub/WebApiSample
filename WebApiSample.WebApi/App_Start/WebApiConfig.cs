d2ab11378986aebf1267b04ec3bd5cd938430f57
ThiagoAbucarub <thiago.abucarub@gmail.com>
2018-02-02 18:25:32 -0200

    XML to JSON
    new code for convert XML to JSON on WebApiConfig.cs

--- WebApiSample.WebApi/App_Start/WebApiConfig.cs
+++ WebApiSample.WebApi/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebApiSample.WebApi
@@ -17,8 +18,11 @@ namespace WebApiSample.WebApi
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new { id = RouteParameter.Optional });
+
+            // XML to JSON
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            
         }
     }
 }
